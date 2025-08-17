cff267233fe81aa7bf01bb4cde2abc8b7c9ef602
[ISW] <is@israelswilson.com>
2025-08-17 19:16:57 -0400

    Add Winston logger utility with production file logging

---
+++ src/utils/logger.ts
@@ -0,0 +1,27 @@
+import winston from 'winston';
+import config from './config';
+
+const logger = winston.createLogger({
+  level: config.env === 'production' ? 'info' : 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
+
+if (config.env === 'production') {
+  logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }));
+  logger.add(new winston.transports.File({ filename: 'combined.log' }));
+}
+
+export default logger;
